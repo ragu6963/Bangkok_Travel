c22712215039f0728f0aa2e2d7d2cf30b2924194
ragu6963 <bhj1684@naver.com>
2021-03-15 01:20:37 +0900

    2021 03 15 Separation SECRET_KEY, Street View Test

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.conf import settings
 
 def home(request):
+    MAPS_API_KEY = settings.MAPS_API_KEY
+    context={
+        "MAPS_API_KEY":MAPS_API_KEY
+    }
 
-    return render(request, "posts/home.html")
+    return render(request, "posts/home.html",context)
--- sharestreetview/settings.py
+++ sharestreetview/settings.py
@@ -11,16 +11,26 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os, json
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') 
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "j-f-sp6hhfq4-4_1_u=x0$z$0q12+*nz!_5t^dyeqwc@i++n$m"
+SECRET_KEY = secrets["SECRET_KEY"]
+MAPS_API_KEY = secrets["MAPS_API_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
