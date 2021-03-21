6eda5f49c18a57d4a31e7cd14ae513b9132b5749
ragu6963 <bhj1684@naver.com>
2021-03-22 00:02:05 +0900

    forms 수정

--- posts/forms.py
+++ posts/forms.py
@@ -3,9 +3,22 @@ from ckeditor.widgets import CKEditorWidget
 
 
 class PostForm(forms.Form):
-    title = forms.CharField(max_length=100)
+    title = forms.CharField(
+        max_length=100,
+        label="",
+        label_suffix="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "제목을 입력하세요",
+            }
+        ),
+    )
     # content = forms.CharField(widget=forms.Textarea)
-    content = forms.CharField(widget=CKEditorWidget())
+    content = forms.CharField(
+        label="",
+        label_suffix="",
+        widget=CKEditorWidget(),
+    )
 
     CHOICES = [
         ("asia", "아시아"),
@@ -15,5 +28,13 @@ class PostForm(forms.Form):
         ("oceania", "오세아니아"),
         ("africa", "아프리카"),
     ]
-    category = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
-    url = forms.URLField()
+    # category = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
+    url = forms.URLField(
+        label="",
+        label_suffix="",
+        widget=forms.URLInput(
+            attrs={
+                "placeholder": "구글 스트리트뷰 주소를 입력해주세요",
+            }
+        ),
+    )
